aa6ac90c74f62f85d0188fb48f0c44a70d729fe4
Cindy Planchenault <cnd.planchenault@gmail.com>
2020-01-29 15:07:45 +0100

    faker

--- src/DataFixtures/EventFixtures.php
+++ src/DataFixtures/EventFixtures.php
@@ -2,6 +2,7 @@
 
 namespace App\DataFixtures;
 
+use Faker;
 use App\Entity\Event;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Common\Persistence\ObjectManager;
@@ -10,11 +11,16 @@ class EventFixtures extends Fixture
 {
     public function load(ObjectManager $manager)
     {
-            $event = new Event();
-            $event->setName('Mon spectacle');
-            $event->setDescription('Ma description');
-            $event->setDuration('2');
-            $manager->persist($event);
+            $faker = Faker\Factory::create('fr_FR');
+
+            for ($i=0; $i<20; $i++){
+                $event = new Event();
+                $event->setName($faker->word);
+                $event->setDescription($faker->paragraph);
+                $event->setDuration($faker->numberBetween(30, 120));
+                $manager->persist($event);
+            }
+
 
         $manager->flush();
     }
