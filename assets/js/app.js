429e158297c6db8986b3da59373ced8d5e5b2f7a
Cindy Planchenault <cnd.planchenault@gmail.com>
2020-01-29 11:27:15 +0100

    installation bootstrap et webpack

--- assets/js/app.js
+++ assets/js/app.js
@@ -5,10 +5,21 @@
  * (and its CSS file) in your base layout (base.html.twig).
  */
 
-// any CSS you import will output into a single css file (app.css in this case)
-import '../css/app.css';
+// any CSS you require will output into a single css file (app.css in this case)
+require('../scss/app.scss');
+
+// this "modifies" the jquery module: adding behavior to it
+// the bootstrap module doesn't export/return anything
+// eslint-disable-next-line import/no-extraneous-dependencies
+require('bootstrap');
+
+
+// Need jQuery? Install it with "yarn add jquery", then uncomment to require it.
+// eslint-disable-next-line import/no-extraneous-dependencies,no-unused-vars
+const $ = require('jquery');
 
 // Need jQuery? Install it with "yarn add jquery", then uncomment to import it.
 // import $ from 'jquery';
 
 console.log('Hello Webpack Encore! Edit me in assets/js/app.js');
+
