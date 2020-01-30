f4e4bd51c3ce6662356dcbafe99df7b39fb3f668
Cindy Planchenault <cnd.planchenault@gmail.com>
2020-01-30 17:21:30 +0100

    index artists

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Repository\ArtistRepository;
 use App\Repository\EventRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
@@ -11,10 +12,14 @@ class HomeController extends AbstractController
     /**
      * @Route("/home", name="home")
      */
-    public function index(EventRepository $eventRepository)
+    public function index(EventRepository $eventRepository, ArtistRepository $artistRepository)
     {
+        $artists = $artistRepository->findAll();
+        shuffle($artists);
+        $someArtists = array_slice($artists, 0, 3);
         return $this->render('home/index.html.twig', [
             'events' => $eventRepository->findBy([], [], 3),
+            'artists' => $someArtists,
         ]);
     }
 }
