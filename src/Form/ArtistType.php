97503f0175e1ac664cd4b03bf06952245eaf2107
Cindy Planchenault <cnd.planchenault@gmail.com>
2020-01-30 16:03:20 +0100

    artists admin

--- src/Form/ArtistType.php
+++ src/Form/ArtistType.php
@@ -3,7 +3,10 @@
 namespace App\Form;
 
 use App\Entity\Artist;
+use App\Entity\Event;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
@@ -12,9 +15,16 @@ class ArtistType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
-            ->add('name')
-            ->add('performance')
-            ->add('events')
+            ->add('name', TextType::class, ['label' => 'Nom'])
+            ->add('performance', TextType::class, ['label' => 'Performance'])
+            ->add('picture', TextType::class, ['label' => 'Photo'])
+            ->add('events', EntityType::class, [
+                'class' => Event::class,
+                'label' => 'Spectacles',
+                'choice_label' => 'name',
+                'expanded' => true,
+                'multiple' => true,
+            ])
         ;
     }
 
